54c99dbe2a0584c4b3832a73f16a4f426a5ff1f9
David A. Wheeler <dwheeler@dwheeler.com>
2022-07-22 13:38:16 -0400

    Add missing .sh
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- run-regenerate.sh
+++ run-regenerate.sh
@@ -2,5 +2,5 @@
 # run-regenerate - run regenerate-website as user "generator"
 # This is helpful in debugging run-regenerate
 
-cp -p /root/regenerate-website /home/generator/
-runuser -u generator -- '/home/generator/regenerate-website'
+cp -p /root/regenerate-website.sh /home/generator/
+runuser -u generator -- '/home/generator/regenerate-website.sh'

38b4224913cd83e0b44f4ae7c8d0646703ab6d32
xamidi <samiro.discher@rwth-aachen.de>
2024-06-14 01:52:36 +0200

    comment pdflatex rerun
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- build-website.sh
+++ build-website.sh
@@ -141,7 +141,7 @@ cp ../repos/metamath-book/narrow.sty ../repos/metamath-book/normal.sty latex
 cd latex
   for file in finiteaxiom megillaward2003 megillaward2004; do
     pdflatex $file
-    pdflatex $file
+    pdflatex $file  # rerun to get references right
     mv $file.pdf ../downloads
   done
   rm *.aux *.sty # keep *.log
